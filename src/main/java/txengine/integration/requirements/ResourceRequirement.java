36d404f0b6c648ea2f92b865952a0011a0bc176a
AaronSigal <asigal55@gmail.com>
2021-09-11 22:43:47 -0500

    Fixed ResourceRequirement bug, refactored Load

--- src/main/java/txengine/integration/requirements/ResourceRequirement.java
+++ src/main/java/txengine/integration/requirements/ResourceRequirement.java
@@ -26,10 +26,10 @@ public class ResourceRequirement extends Requirement {
 
         for (AbstractMap.SimpleEntry<String, Integer> pair : Utils.parseStringIntPairs(List.of(properties))) {
             sb.append(pair.getValue()).append(" ").append(pair.getKey());
-            sb.append(",");
+            sb.append(", ");
         }
 
-        sb.replace(sb.length()-1, sb.length()-1, ".");
+        sb.replace(sb.length() -2, sb.length() -1, ".");
 
         return sb.toString();
     }
